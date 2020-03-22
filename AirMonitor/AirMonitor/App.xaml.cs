7774f412681c445a4b27419298ddaa4a28f11943
Karolina Kania <karolina.kania@microsoft.wsei.edu.pl>
2020-03-22 17:55:08 +0100

    changes

--- AirMonitor/AirMonitor/App.xaml.cs
+++ AirMonitor/AirMonitor/App.xaml.cs
@@ -11,7 +11,7 @@ namespace AirMonitor
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new HomePage();
         }
 
         protected override void OnStart()
---
+++ AirMonitor/AirMonitor/ViewModels/HomeViewModel.cs
@@ -0,0 +1,29 @@
+﻿using AirMonitor.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace AirMonitor.ViewModels
+{
+    class HomeViewModel
+    {
+        private readonly INavigation _navigation;
+
+        public HomeViewModel(INavigation navigation)
+        {
+            _navigation = navigation;
+        }
+
+        private ICommand _gotToDetailsCommand;
+
+        public ICommand GoToDetailsCommand => _gotToDetailsCommand ?? (_gotToDetailsCommand = new Command(OnGoToDetails));
+
+        private void OnGoToDetails()
+        {
+            _navigation.PushAsync(new HomePage());
+        }
+    }
+
+}
